9c1a2c09844441d564b33567e6f322c0d6db462a
Harry Craven <HarryCraven.Dev@gmail.com>
2020-09-24 17:02:36 +0100

    Update to server.

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // Load dotenv
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount router
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `App running in ${process.env.NODE_ENV} mode and on port ${PORT}!`
+      .brightYellow.bold
   );
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.brightRed.bold);
+  server.close(() => process.exit(1));
 });
