74b377f29b4e37e13a7b9d4d72bc243b436c446f
Harry Craven <HarryCraven.Dev@gmail.com>
2020-09-25 16:51:30 +0100

    Adding geocoder to update location value.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -102,6 +103,23 @@ const BootcampSchema = new mongoose.Schema({
 // Create bootcamp Slug from the name
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].street,
+    city: loc[0].city,
+    zipcode: loc[0].zipcode,
+    country: loc[0].country,
+  };
+  // Do not save to DB
+  this.address = undefined;
   next();
 });
 
